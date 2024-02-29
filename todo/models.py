e7ba9f370378b9d90d76fb40529612c9ea02afe0
hexaalif <shadullahsad9@gmail.com>
2024-03-01 02:45:30 +0600

    first commit

---
+++ todo/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+PRIORITY_CHOICES=[
+    ('Regular', 'Regular'),
+    ('Moderate', 'Moderate'),
+    ('Important', 'Important'),
+    ('Urgent', 'Urgent'),
+    ('Very Urgent', 'Very Urgent'),
+]
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    completed = models.BooleanField(default=False)
+    date=models.DateField(auto_now_add = True)
+    priority = models.CharField(choices=PRIORITY_CHOICES,max_length=20, default='Regular')
+
+    def __str__(self):
+        return self.title
+    
+class Profile(models.Model):
+    user= models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
---
+++ todo/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render,redirect
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo 
+from . import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
+from rest_framework.authtoken.models import Token
+
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+# Create your views here.
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all().order_by("-id")
+
+class UserRegistrationApiView(APIView):
+    serializer_class = serializers.RegistrationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            print(user)
+            token = default_token_generator.make_token(user)
+            print(token)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            print(uid)
+            confirm_link = f"http://127.0.0.1:8000/todo/active/{uid}/{token}"
+            email_subject="Confirm your Email"
+            email_body= render_to_string('confirm_email.html', {'confirm_link': confirm_link})
+            email = EmailMultiAlternatives(email_subject, '', to=[user.email])
+            email.attach_alternative(email_body, "text/html")
+            email.send()
+            return Response("Check Your email for confirmation")
+        return Response(serializer.errors)
+    
+def activate(req, uid64, token):
+    try:
+        uid=urlsafe_base64_decode(uid64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(User.DoesNotExist):
+        user=None 
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active=True
+        user.save()
+        return redirect('login')
+    else:
+        return redirect('register')
+    
+class UserLoginApiView(APIView):
+    def post(self,request):
+        serializer = serializers.UserLoginSerializers(data = self.request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+
+            user=authenticate(username=username, password=password)
+            login(request, user)
+
+            if user:
+                token, _=Token.objects.get_or_create(user=user)
+                return Response({'token': token.key, 'user_id': user.id})
+            else:
+                return Response({"error": "invalid Credential"})
+        return Response(serializer.errors)
+    
+class UserLogoutView(APIView):
+    def get(self, req):
+        req.user.auth_token.delete()
+        logout(req)
+        return redirect('login')
